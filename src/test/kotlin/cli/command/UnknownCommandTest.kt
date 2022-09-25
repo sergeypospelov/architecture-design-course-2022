e8daf3fc73f8f237f7e2306d533cd7a7b5baef6a
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-25 15:34:32 +0300

    add UnknownCommand test

---
+++ src/test/kotlin/cli/command/UnknownCommandTest.kt
@@ -0,0 +1,43 @@
+package cli.command
+
+import cli.command.TestUtil.convertToString
+import org.apache.commons.lang3.SystemUtils
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+
+class UnknownCommandTest {
+
+    @Test
+    fun `should show contents of the current directory`() {
+        val name = if (SystemUtils.IS_OS_WINDOWS) "dir" else "ls"
+        val arguments = listOf<String>()
+        val outputStream = ByteArrayOutputStream()
+        val exitCode = UnknownCommand(name, arguments)
+            .execute(InputStream.nullInputStream(), outputStream)
+        assertEquals(0, exitCode)
+    }
+
+    @Test
+    fun `should call python`() {
+        val name = "python"
+        val arguments = listOf("./src/test/resources/script.py")
+        val outputStream = ByteArrayOutputStream()
+        val exitCode = UnknownCommand(name, arguments)
+            .execute(InputStream.nullInputStream(), outputStream)
+        assertEquals(0, exitCode)
+        assertEquals("hello", outputStream.convertToString())
+    }
+
+    @Test
+    fun `should fail on the unknown command`() {
+        val name = "unknown_command_228"
+        val arguments = listOf<String>()
+        val exitCode = UnknownCommand(name, arguments)
+            .execute(InputStream.nullInputStream(), OutputStream.nullOutputStream())
+        Assertions.assertNotEquals(0, exitCode)
+    }
+}
